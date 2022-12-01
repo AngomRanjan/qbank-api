fe38f29d29f799981c560f7260d7b864acb45332
AngomRanjan <angomranjan@gmail.com>
2022-12-02 03:14:18 +0530

    update devise-user migration

--- db/migrate/20221201211422_devise_create_users.rb
+++ db/migrate/20221201211422_devise_create_users.rb
@@ -3,6 +3,8 @@
 class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
+      t.string :username, null: false
+      t.timestamps
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
