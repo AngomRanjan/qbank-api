00e01eb8ed04aa89826d56fe68daaf2e29e323b2
AngomRanjan <angomranjan@gmail.com>
2022-12-02 04:32:40 +0530

    Add User jwt secret key in devise.rb, corrected registrations route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
   }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
